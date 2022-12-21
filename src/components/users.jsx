27886a90e8c07ccc1b42bb06bab9d05cee36cb21
Egor Makarov <egor.makarov@gmail.com>
2022-12-21 09:17:06 +0300

    render user table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -3,6 +3,7 @@ import api from '../api'
 
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll())
+
     const handleDelete = (userId) => {
 
     }
@@ -11,13 +12,51 @@ const Users = () => {
 
     }
 
-    const renderTable = () => {
-        users.map((item) => {
-
-        })
+    const renderQualities = (qualities) => {    
+        return qualities.map((qualitie) =>             
+            <span key={qualitie._id} className={'m-2 badge bg-' + qualitie.color}>{qualitie.name}</span>
+        )
     }
 
+    const renderProfession = (professions) => {
+        return <span key={professions._id}>{professions.name}</span>        
+    }
+    
+
+    const renderTable = () => {
+        return users.map((user) => 
+
+            <tr key={user._id} data-bookmark={user.bookmark}>                
+                <th >{user.name}</th>  
+                <td>{renderQualities(user.qualities)}</td>  
+                <td>{renderProfession(user.profession)}</td>   
+                <td>{user.completedMeetings}</td>  
+                <td>{user.rate}</td>  
+                <td><button className='btn btn-danger'>delete</button></td>
+            </tr>
+        )
+
+    }
+
+
     return <>
+    
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретился, раз</th>
+                    <th scope="col">Оценка</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderTable()}
+            </tbody>
+        </table>
+
     </>
 }
 
