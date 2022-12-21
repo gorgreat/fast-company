aa8591c900db39b2c6a855908442ee7784c017da
Egor Makarov <egor.makarov@gmail.com>
2022-12-21 11:09:58 +0300

    delete user, declensions, count

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,12 +4,19 @@ import api from '../api'
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll())
 
-    const handleDelete = (userId) => {
-
+    const declensions = (number, titles) => {
+        let cases = [2, 0, 1, 1, 1, 2]
+        return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]]
     }
-    //количество user
-    const renderPhrase = (number) => {
 
+    const handleDelete = (userId) => {       
+        setUsers((prevState) => prevState.filter((user) => user !== userId))
+    }
+
+    const renderPhrase = (number) => {
+        return number === 0 ? 
+            <h1> <span className='badge bg-danger'>никто не тусанет с тобой, дружище </span></h1> : 
+            <h1> <span className='badge bg-primary'>{number} {declensions(number,['человек', 'человека', 'человек'])} тусанет с тобой сегодня </span></h1>
     }
 
     const renderQualities = (qualities) => {    
@@ -22,25 +29,21 @@ const Users = () => {
         return <span key={professions._id}>{professions.name}</span>        
     }
     
-
     const renderTable = () => {
-        return users.map((user) => 
-
+        return users.map((user) =>             
             <tr key={user._id} data-bookmark={user.bookmark}>                
                 <th >{user.name}</th>  
                 <td>{renderQualities(user.qualities)}</td>  
                 <td>{renderProfession(user.profession)}</td>   
                 <td>{user.completedMeetings}</td>  
                 <td>{user.rate}</td>  
-                <td><button className='btn btn-danger'>delete</button></td>
+                <td><button className='btn btn-danger' onClick={() => handleDelete(user)}>delete</button></td>
             </tr>
         )
-
     }
 
-
     return <>
-    
+        {renderPhrase(users.length)}
         <table className="table">
             <thead>
                 <tr>
@@ -56,7 +59,6 @@ const Users = () => {
                 {renderTable()}
             </tbody>
         </table>
-
     </>
 }
 
